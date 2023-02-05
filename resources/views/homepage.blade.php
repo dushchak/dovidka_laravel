8a47d13769b011bd81d3b0bbf415453e2d19c978
dushchak <dvolodymyr@gmail.com>
2023-02-05 23:50:11 +0200

    Начало проекта

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,7 @@
+@extends('template')
+
+@section('main_content')
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex quibusdam illo quidem sed repellat, ducimus ipsa eius alias quasi soluta adipisci pariatur quo mollitia magnam ut, et voluptate enim.
+
+
+@endsection
