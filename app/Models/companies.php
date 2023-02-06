e1f1efc4e25b940e8af37cdedb0a4058880985fa
dushchak <dvolodymyr@gmail.com>
2023-02-06 21:43:25 +0200

    select * from companies

--- app/Models/companies.php
+++ app/Models/companies.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class companiesModel extends Model
+class companies extends Model
 {
     use HasFactory;
+    
 }
